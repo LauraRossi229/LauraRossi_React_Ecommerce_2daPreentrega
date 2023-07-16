d55bb0ec0b35218b5bc0196248ccd087fd527c05
LauraRossi229 <122574291+LauraRossi229@users.noreply.github.com>
2023-07-16 00:52:29 -0300

    Update CheckoutForm.jsx
    Correción Aspecto

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -9,6 +9,12 @@ const CheckoutForm = ({ onConfirm }) => {
   const handleConfirm = (event) => {
     event.preventDefault();
 
+    if (name.trim() === "" || phone.trim() === "" || email.trim() === "") {
+      // Validar que todos los campos estén completos
+      alert("Por favor, complete todos los campos del formulario.");
+      return;
+    }
+
     const userData = {
       name,
       phone,
@@ -21,37 +27,37 @@ const CheckoutForm = ({ onConfirm }) => {
     <Container>
       <Form onSubmit={handleConfirm} className="form">
         <Form.Group controlId="formName">
-          <Form.Label>Name</Form.Label>
+          <Form.Label className="mt-2">Nombre y Apellido</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Enter your name"
+            placeholder="Escriba su nombre y apellido."
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
         </Form.Group>
 
         <Form.Group controlId="formPhone">
-          <Form.Label>Phone</Form.Label>
+          <Form.Label className="mt-2">Teléfono</Form.Label>
           <Form.Control
             type="tel"
-            placeholder="Enter your phone number"
+            placeholder="Escriba su número de teléfono"
             value={phone}
             onChange={(e) => setPhone(e.target.value)}
           />
         </Form.Group>
 
         <Form.Group controlId="formEmail">
-          <Form.Label>Email</Form.Label>
+          <Form.Label className="mt-2">Email</Form.Label>
           <Form.Control
             type="email"
-            placeholder="Enter your email"
+            placeholder="Escriba su correo electrónico"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Confirm
+        <Button variant="primary" type="submit" className="mt-5">
+          Confirmar Compra
         </Button>
       </Form>
     </Container>
